6954a04367f2c9bd5b0b1e5b426a795844ffc9f2
SubhraS-G <samalsubhrajit488@gmail.com>
2026-03-06 23:49:15 +0530

    Initialize main.jsx with React and SubhraGaming component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SubhraGaming from './SubhraGaming'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SubhraGaming />
+  </React.StrictMode>
+)
